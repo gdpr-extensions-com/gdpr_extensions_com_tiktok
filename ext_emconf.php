14351caf3f3ed3e4a6d45ea14cd5ba14865771e8
gdpr-extensions-com <support@gdpr-extensions.com>
2024-05-31 01:06:58 +0500

    description added in emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,16 +2,16 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'GDPR-Extensions-com - TikTok 2xClick Solution',
-    'description' => '',
+    'description' => 'Add TikTok videos to your website and stay GDPR compliant! Your website viewers will be able to manage their cookie preferences and view the content only if consented. Stay GDPR compliant when your website is scanned by any Cookie or GDPR Scanner!',
     'category' => 'plugin',
     'author' => '',
     'author_email' => '',
-    'state' => 'alpha',
+    'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
